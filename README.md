852cad3f71df0c981bcd5112ceacb52040d2a811
vinid <chiccobia@gmail.com>
2023-05-29 11:58:04 -0700

    minor updates

---
+++ README.md
@@ -0,0 +1,32 @@
+# Deploying A Fashion Search Engine from Scratch with Beam and FashionCLIP
+
+
+## Introduction
+
+This is the code for the following [blog post]().
+Best way to use this is to first read the post; it will walk you through the process of deploying a fashion search engine from scratch using Beam and FashionCLIP.
+
+This readme contains basic information on how to install Beam and deploy the search engine.
+
+## Basic Installation
+    
+To install Beam. You can run the following commands in your terminal:
+```bash
+    curl https://raw.githubusercontent.com/slai-labs/get-beam/main/get-beam.sh -sSfL | sh
+    beam configure
+    pip install beam-sdk
+```
+
+## Creating Volumes and Uploading Data
+
+```bash
+  beam volume upload cache_data fashionbeam_data -a fashion-clip-app
+```
+
+## Deploying the Search Engine
+
+```bash
+    beam deploy app.py
+```
+
+
--- webapp/run.py
+++ webapp/run.py
@@ -30,32 +30,41 @@ def load_or_create_embeddings(fclip):
         with open(EMBEDDING_PATH, "rb") as f:
             image_embeddings = pickle.load(f)
     else:
-        subset = pd.read_csv("/workspace/cache_data/new_dataset/subset.csv")
-
-        # add path to images
+        # first we get all the paths of the images
         images = [f"/workspace/cache_data/new_dataset/{k}.jpg" for k in subset["article_id"].tolist()]
+
+        # then we embed them with fashion clip (batch size 32) and we normalize the embeddings
         image_embeddings = fclip.encode_images(images, 32)
         image_embeddings = image_embeddings / np.linalg.norm(image_embeddings, ord=2, axis=-1, keepdims=True)
 
         with open(EMBEDDING_PATH, "wb") as f:
-            pickle.dump(image_embeddings, f)
+            pickle.dump(image_embeddings, w)
 
     return image_embeddings
 
-def search_image(**inputs):
 
+def search_image(**inputs):
+    # loading the model (should be cached thanks to the loader)
     fclip = load_fclip()
+
+    # loading the csv of the data
     subset = pd.read_csv("/workspace/cache_data/new_dataset/subset.csv")
+
+    # loading the image embeddings
     image_embeddings = load_or_create_embeddings(fclip)
 
+    # retrieving the query from the inputs
     query = inputs["query"]
 
+    # create embeddings of the query (batch size 32 even if not necessary) and normalize it
     text_embedding = fclip.encode_text([query], 32)[0]
-    text_embedding = text_embedding/np.linalg.norm(text_embedding)
+    text_embedding = text_embedding / np.linalg.norm(text_embedding)
 
+    # dot product to find the matching object
     id_of_matched_object = np.argmax(text_embedding.dot(image_embeddings.T))
     found_object = subset["article_id"].iloc[id_of_matched_object].tolist()
 
+    # finally we save the image
     image = Image.open("/workspace/cache_data/new_dataset/" + str(found_object) + ".jpg")
 
     image.save("output.png")
